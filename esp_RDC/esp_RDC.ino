1b3312e193ff9c2b52420b0a1a22a1dc909f0016
Matthh9 <95867037+Matthh9@users.noreply.github.com>
2023-01-12 00:05:40 +0100

    Update esp_RDC.ino

--- esp_RDC/esp_RDC.ino
+++ esp_RDC/esp_RDC.ino
@@ -22,81 +22,117 @@ int BRIGHTNESS=255;
 
 CRGB leds[NUM_LEDS];
 
-
 #define port_sonnette 12
+#define port_piezo 26
 
+bool lumiere_on_off = true;
+bool sonnette_off = true;
 
 /**********************************************************************************
  * SECTION DES FONCTIONS
  *********************************************************************************/
 void sonnette(void * parameter){
-    // Change this depending on where you put your piezo buzzer
-    const int TONE_OUTPUT_PIN = 26;
-    
-    // The ESP32 has 16 channels which can generate 16 independent waveforms
-    // We'll just choose PWM channel 1 here
-    const int TONE_PWM_CHANNEL = 1; 
-    
-    // change this to make the song slower or faster
-    int tempo = 144;
-
-    note_t notemusique [] =  {
-      NOTE_D, NOTE_G, NOTE_Bb, NOTE_A,
-      NOTE_G, NOTE_D, NOTE_C, NOTE_A,
-    };
-    
-    int octave [] = { //uint8_t
-      4, 4, 4, 4, 
-      4, 5, 5, 4,
-    };
-    
-    int temp[] = {
-      4, -4, 8, 4, 
-      2, 4, -2, -2,
-    };
-
-    int notes = sizeof(notemusique) / sizeof(notemusique[0]);
-
-    // this calculates the duration of a whole note in ms (60s/tempo)*4 beats
-    int wholenote = (60000 * 4) / tempo;
-    
-    int divider = 0, noteDuration = 0;
-
-    ledcAttachPin(TONE_OUTPUT_PIN, TONE_PWM_CHANNEL);
-
-    for (int thisNote = 0; thisNote < notes; thisNote = thisNote + 1) {
-      // calculates the duration of each note
-      divider = temp[thisNote];
-      if (divider > 0) {
-        // regular note, just proceed
-        noteDuration = (wholenote) / divider;
-      } else if (divider < 0) {
-        // dotted notes are represented with negative durations!!
-        noteDuration = (wholenote) / abs(divider);
-        noteDuration *= 1.5; // increases the duration in half for dotted notes
-      }
-      // we only play the note for 90% of the duration, leaving 10% as a pause
-      ledcWriteNote(TONE_PWM_CHANNEL, notemusique[thisNote], octave[thisNote]);
+  client.publish("commande", "sonnette");
+  // Change this depending on where you put your piezo buzzer
+  //const int TONE_OUTPUT_PIN = port_piezo;
   
-      
-      // Wait for the specief duration before playing the next note.
-      vTaskDelay(noteDuration*0.9 / portTICK_PERIOD_MS);
-      ledcWriteTone(TONE_PWM_CHANNEL, 0);
-      vTaskDelay(noteDuration*0.1 / portTICK_PERIOD_MS);
-      
-    }
+  // The ESP32 has 16 channels which can generate 16 independent waveforms
+  // We'll just choose PWM channel 1 here
+  const int TONE_PWM_CHANNEL = 1; 
+  
+  // change this to make the song slower or faster
+  int tempo = 144;
 
-    ledcDetachPin(TONE_OUTPUT_PIN);
+  note_t notemusique [] =  {
+    NOTE_D, NOTE_G, NOTE_Bb, NOTE_A,
+    NOTE_G, NOTE_D, NOTE_C, NOTE_A,
+  };
+  
+  int octave [] = { //uint8_t
+    4, 4, 4, 4, 
+    4, 5, 5, 4,
+  };
+  
+  int temp[] = {
+    4, -4, 8, 4, 
+    2, 4, -2, -2,
+  };
+
+  int notes = sizeof(notemusique) / sizeof(notemusique[0]);
+  // this calculates the duration of a whole note in ms (60s/tempo)*4 beats
+  int wholenote = (60000 * 4) / tempo;
+  int divider = 0, noteDuration = 0;
+
+  ledcAttachPin(port_piezo, TONE_PWM_CHANNEL);
+
+  for (int thisNote = 0; thisNote < notes; thisNote = thisNote + 1) {
+    // calculates the duration of each note
+    divider = temp[thisNote];
+    if (divider > 0) {
+      // regular note, just proceed
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted notes are represented with negative durations!!
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5; // increases the duration in half for dotted notes
+    }
+    // we only play the note for 90% of the duration, leaving 10% as a pause
+    ledcWriteNote(TONE_PWM_CHANNEL, notemusique[thisNote], octave[thisNote]);
+
+    // Wait for the specief duration before playing the next note.
+    vTaskDelay(noteDuration*0.9 / portTICK_PERIOD_MS);
+    ledcWriteTone(TONE_PWM_CHANNEL, 0);
+    vTaskDelay(noteDuration*0.1 / portTICK_PERIOD_MS);
     
-    //envoie d'un message mqtt pour
-    
-    
-    // When you're done, call vTaskDelete. Don't forget this!
-    vTaskDelete(NULL);
+  }
+
+  ledcDetachPin(port_piezo);
+
+  
+  digitalWrite(port_sonnette, LOW);
+  vTaskDelay(3000 / portTICK_PERIOD_MS);
+  
+  sonnette_off=true;
+  
+  // When you're done, call vTaskDelete. Don't forget this!
+  vTaskDelete(NULL);
 }
 
 
 void demarrage_projo(void * parameter){
+  Serial.print("projo task");
+//  if defined(ESP32)
+//  #define IR_RECEIVE_PIN          15  // D15
+//  #define IR_SEND_PIN              4  // D4
+//  #define tone(a,b,c) void()      // no tone() available on ESP32
+//  #define noTone(a) void()
+//  #define TONE_PIN                42 // Dummy for examples using it
+//  #define APPLICATION_PIN         16 // RX2 pin
+
+  digitalWrite(projo_power, HIGH);
+  vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+  // Implement your custom logic here
+  IrSender.begin(IR_SEND_PIN, false);// Specify send pin and enable feedback LED at default feedback LED pin
+  
+  uint16_t sAddress = 0x0102;
+  uint8_t sCommand = 0x34;
+  uint8_t sRepeats = 0;
+  
+  // Results for the first loop to: Protocol=NEC Address=0x102 Command=0x34 Raw-Data=0xCB340102 (32 bits)
+  //IrSender.sendNEC(sAddress, sCommand, sRepeats);
+  IrSender.sendNECRaw(0xCB340102, sRepeats);
+  vTaskDelay(5000 / portTICK_PERIOD_MS);
+  IrSender.sendNECRaw(0xCB340102, sRepeats); //son up pour forcer l'activation bug du projo
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  IrSender.sendNECRaw(0xCB340102, sRepeats); //son down pour refarie l'équilibre
+
+// When you're done, call vTaskDelete. Don't forget this!
+  vTaskDelete(NULL);
+}
+
+
+void extinction_projo(void * parameter){
   Serial.print("projo task");
 //  if defined(ESP32)
 //  #define IR_RECEIVE_PIN          15  // D15
@@ -114,12 +150,12 @@ void demarrage_projo(void * parameter){
   uint8_t sRepeats = 0;
   
   // Results for the first loop to: Protocol=NEC Address=0x102 Command=0x34 Raw-Data=0xCB340102 (32 bits)
-  IrSender.sendNEC(sAddress, sCommand, sRepeats);
-//  IrSender.sendNECRaw(0xCB340102, sRepeats);
+  //IrSender.sendNEC(sAddress, sCommand, sRepeats);
+  IrSender.sendNECRaw(0xCB340102, sRepeats);
+  vTaskDelay(10000 / portTICK_PERIOD_MS);
+  digitalWrite(projo_power, LOW);
   
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-  
-  // When you're done, call vTaskDelete. Don't forget this!
+// When you're done, call vTaskDelete. Don't forget this!
   vTaskDelete(NULL);
 }
 
@@ -163,48 +199,36 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println();
 
   if (String(topic).equals("commande") == 1) {
-    //lave linge
+
     if (message.indexOf("{\"idx\" : 1") != -1 ) {
-      Serial.println("Lave linge");
-      if (message.indexOf("\"nvalue\" : 3") != -1) {
-        //on allume la prise de la machine
-        //digitalWrite(lave_linge_power, HIGH);
-        delay(5000);
-        //bouton(lave_linge_start);
-      } else if (message.indexOf("\"nvalue\" : 0") != -1) {
-        //on éteint la prise de la machine
-        //digitalWrite(lave_linge_power, LOW);
-      }
-    }
-
-    if (message.indexOf("{\"idx\" : 3") != -1 ) {
-      Serial.println("sonette");
-      if (message.indexOf("\"nvalue\" : 1") != -1) {
-        
-      } else if (message.indexOf("\"nvalue\" : 0") != -1) {
-      }
-    }
-
-    if (message.indexOf("{\"idx\" : 4") != -1 ) {
       Serial.println("Projo");
-      if (message.indexOf("\"nvalue\" : 1") != -1) {
-          
+      if (message.indexOf("\"nvalue\" : 3") != -1) {
         xTaskCreatePinnedToCore(
-            demarrage_projo,    // Function that should be called
-            "demarrage_projo",   // Name of the task (for debugging)
-            1000,            // Stack size (bytes)
-            NULL,            // Parameter to pass
-            2,               // Task priority
-            NULL,             // Task handle
-            1          // Core you want to run the task on (0 or 1)
-          );
-        
-
+          demarrage_projo,    // Function that should be called
+          "demarrage_projo",   // Name of the task (for debugging)
+          1000,            // Stack size (bytes)
+          NULL,            // Parameter to pass
+          2,               // Task priority
+          NULL,             // Task handle
+          1          // Core you want to run the task on (0 or 1)
+        );
+          
       } else if (message.indexOf("\"nvalue\" : 0") != -1) {
-
+        xTaskCreatePinnedToCore(
+          extinction_projo,    // Function that should be called
+          "extinction_projo",   // Name of the task (for debugging)
+          1000,            // Stack size (bytes)
+          NULL,            // Parameter to pass
+          2,               // Task priority
+          NULL,             // Task handle
+          1          // Core you want to run the task on (0 or 1)
+        );
       }
     }
-    
+
+
+
+
   }
 }
 
@@ -230,31 +254,6 @@ void reconnect() {
       // Wait 5 seconds before retrying
       delay(5000);
     }
-  }
-}
-
-
-#define led1 32
-#define led2 27
-
-// task to be delayed
-void vATask1( void * pvParameters )
-{
-  for(;;){ // infinite loop
-
-    // Turn the LED on
-    digitalWrite(led1, HIGH);
-
-    // Pause the task for 500ms
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-    //delay(1000);
-    
-    // Turn the LED off
-    digitalWrite(led1, LOW);
-
-    // Pause the task again for 500ms
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-    //delay(1000);
   }
 }
 
@@ -299,14 +298,16 @@ void pride()
     
     nblend( leds[pixelnumber], newcolor, 64);
   }
-  
+
+  FastLED.show();
 }
 
+void reconnect();
 
 void setup() {
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-
+  pinMode(port_sonnette, OUTPUT);
+  digitalWrite(port_sonnette, LOW);
+  pinMode(port_sonnette, INPUT);
 
   // tell FastLED about the LED strip configuration
   FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS)
@@ -315,48 +316,36 @@ void setup() {
 
   // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
-
   
   Serial.begin(9600);
   setup_wifi();
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
-
-
-
-  xTaskCreatePinnedToCore(
-      vATask1,    // Function that should be called
-      "Toggle LED",   // Name of the task (for debugging)
-      1000,            // Stack size (bytes)
-      NULL,            // Parameter to pass
-      2,               // Task priority
-      NULL,             // Task handle
-      0          // Core you want to run the task on (0 or 1)
-    );
-    
-
+  
 }
 
-void loop() {
-   //put your main code here, to run repeatedly:
+void loop() {  
+  //put your main code here, to run repeatedly:
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
-
-  pride();
-  FastLED.show();
-    
-  if(digitalRead(port_sonnette)==HIGH) {
-    client.publish("commande", "sonnette");
+  
+  if(lumiere_on_off) {
+    pride();
+  }
+  
+  if(digitalRead(port_sonnette)==HIGH and sonnette_off) {
+    sonnette_off=false;
     xTaskCreatePinnedToCore(
         sonnette,    // Function that should be called
         "sonette",   // Name of the task (for debugging)
-        1000,            // Stack size (bytes)
+        10000,            // Stack size (bytes)
         NULL,            // Parameter to pass
         1,               // Task priority
         NULL,             // Task handle
         1          // Core you want to run the task on (0 or 1)
       );
   }
+  
 }
